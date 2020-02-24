6944ad5cd1f68025dc68f388e22a795e3b8b7d91
ljmartin <lewis.martin44@gmail.com>
2020-02-24 16:05:06 +1100

    large fp_comparison on 10k set

--- code/similarity_comparison_figures.py
+++ code/similarity_comparison_figures.py
@@ -16,15 +16,21 @@ from tqdm import tqdm
 utils.set_mpl_params()
 fp_names = utils.getNames()
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(1,2)
+fig.set_figheight(7)
+fig.set_figwidth(14)
 
 for fp in fp_names:
     scores = np.load('./processed_data/similarities/'+fp+'_roc.npy')
     low = np.load('./processed_data/similarities/'+fp+'_low.npy')
     high = np.load('./processed_data/similarities/'+fp+'_high.npy')
 
-    ax.plot(scores, label=fp)
+    ax[0].plot(scores, label=fp)
+    ax[1].plot(scores, label=fp)
 
-ax.legend()
-ax.grid()
+ax[1].set_xlim(0,10000)
+ax[0].legend()
+ax[1].legend()
+ax[0].grid()
+ax[1].grid()
 fig.savefig('./processed_data/similarities/roc.png')
