8cd58faf107724bcde0d159c01f598d0b8947003
ljmartin <lewis.martin44@gmail.com>
2020-03-03 15:33:10 +1100

    juggling figures around

--- code/replicate_AVE_figures.py
+++ code/replicate_AVE_figures.py
@@ -59,9 +59,10 @@ plt.close(fig)
 
 
 
-fig, ax = plt.subplots(1,3)
-fig.set_figheight(6)
-fig.set_figwidth(18)
+fig, ax = plt.subplots(2,1)
+fig.set_figheight(7.5)
+fig.set_figwidth(5.5)
+plt.setp(ax, ylim=(-0.2,1.05))
 
 ax[0].scatter(cutoffs, aves_before_trim, alpha=utils.ALPHA, label='Before trim')
 ax[0].scatter(cutoffs, aves_after_trim, alpha=utils.ALPHA, label='After trim')
@@ -73,45 +74,65 @@ utils.plot_fig_label(ax[0], 'A.')
 
 ax[1].scatter(aves_before_trim, aves_after_trim, c=targets, alpha=utils.ALPHA)
 ax[1].plot([0,1],[0,1])
+ax[1].scatter([0],[1.5], alpha=utils.ALPHA, c='C1', label='Target 1')
+ax[1].scatter([0],[1.5], alpha=utils.ALPHA, c='C2', label='Target 2')
+ax[1].scatter([0],[1.5], alpha=utils.ALPHA, c='C3', label='etc...')
 ax[1].set_xlabel('AVE before trimming')
 ax[1].set_ylabel('AVE after trimming')
 ax[1].grid()
+ax[1].legend()
 utils.plot_fig_label(ax[1], 'B.')
-
-ax[2].scatter(aves_before_trim, ap_before_trim, alpha=utils.ALPHA, label='Before trim')
-ax[2].scatter(aves_after_trim, ap_after_trim, alpha=utils.ALPHA, label='After trim')
-ax[2].legend()
-ax[2].set_xlabel('AVE')
-ax[2].set_ylabel('AP')
-ax[2].grid()
-utils.plot_fig_label(ax[2], 'C.')
 
 
 fig.savefig('./processed_data/replicate_AVE/trim.png')
+plt.close(fig)
 
 
-fig, ax = plt.subplots(1,2)
-fig.set_figheight(6)
-fig.set_figwidth(12)
+fig, ax = plt.subplots()
+#This becomes a supplementary:
+ax.scatter(aves_before_trim, ap_before_trim, alpha=utils.ALPHA, label='Before trim')
+ax.scatter(aves_after_trim, ap_after_trim, alpha=utils.ALPHA, label='After trim')
+ax.legend()
+ax.set_xlabel('AVE')
+ax.set_ylabel('AP')
+ax.grid()
+fig.savefig('./processed_data/supplementary/replicate_AVE_vs_AP.png')
+plt.close(fig)
 
 
-ax[0].scatter(aves_before_trim, sizes_before_trim[:,[0,1]].sum(1) / sizes_before_trim[:,[2,3]].sum(1),
-           alpha=utils.ALPHA,
-           label='Before trim')
-ax[0].scatter(aves_after_trim, sizes_after_trim[:,[0,1]].sum(1) / sizes_after_trim[:,[2,3]].sum(1),
-           alpha=utils.ALPHA,
-           label='After trim')
-ax[0].set_xlabel('AVE')
-ax[0].set_ylabel('Test size / Train size')
-ax[0].grid()
-ax[0].legend()
-utils.plot_fig_label(ax[0], 'A.')
-
-kdeplot( np.log10(sizes_before_trim[:,[0,1]].sum(1) / sizes_before_trim[:,[2,3]].sum(1)), ax=ax[1], label='Before trim')
-kdeplot( np.log10(sizes_after_trim[:,[0,1]].sum(1) / sizes_after_trim[:,[2,3]].sum(1)), ax=ax[1], label='After trim')
-ax[1].set_xlabel('Log$_10$(Test size / Train size)')
-ax[1].set_ylabel('Density')
-utils.plot_fig_label(ax[1], 'B')
 
 
-fig.savefig('./processed_data/replicate_AVE/sizes.png')
+
+fig, ax = plt.subplots()
+
+x_points = aves_before_trim.copy()
+xrange = np.linspace(np.min(x_points), np.max(x_points), 10)
+
+def regress(x,y):
+    X = sm.add_constant(x)
+    model = sm.OLS(y, X)
+    result = model.fit()
+    return result
+
+pe1 = (mpe.Stroke(linewidth=5, foreground='black'),
+       mpe.Stroke(foreground='white', alpha=1),
+       mpe.Normal())
+
+for score, label in zip([auroc_before_trim, ap_before_trim], ['AUROC', 'Average precision']):
+#    fudge_factor = score<0.9999
+    fudge_factor = score<0.999999
+    score = score[fudge_factor]
+    x = x_points[fudge_factor].copy()
+    y = np.log10(score / (1-score))
+    result = regress(x,y)
+    ax.plot(xrange, result.params[0]+result.params[1]*xrange, label=label+' Slope:'+str(np.around(result.params[1],3)), path_effects=pe1)
+    ax.scatter(x,y, alpha=utils.ALPHA)
+
+ax.set_xlabel('AVE')
+ax.set_ylabel('Score')
+ax.legend()
+ax.grid()
+
+fig.savefig('./processed_data/supplementary/regression.png')
+plt.close(fig)
+
