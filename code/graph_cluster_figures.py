3bd230a41b5007c49ccd2fd55ec88db33f1008fc
ljmartin <lewis.martin44@gmail.com>
2020-07-23 20:06:11 +1000

    Add files via upload

--- code/graph_cluster_figures.py
+++ code/graph_cluster_figures.py
@@ -32,9 +32,9 @@ aves_after_trim = df['ave_after_trim']
 ap_before_trim = df['ap_before_trim']
 ap_after_trim = df['ap_after_trim']
 
-fig, ax = plt.subplots(2,1)
-fig.set_figheight(7.5)
-fig.set_figwidth(5.5)
+fig, ax = plt.subplots(1,2)
+fig.set_figheight(5)
+fig.set_figwidth(10)
 
 mu_before, sigma_before = norm.fit(aves_before_trim)
 mu_after, sigma_after = norm.fit(aves_after_trim)
@@ -50,14 +50,17 @@ utils.plot_fig_label(ax[0], 'A.')
 
 #for a,b,c,d in zip(aves_before_trim, aves_after_trim, ap_before_trim, ap_after_trim):
 #    ax[1].plot([a,b], [c,d], lw=0.2, c='k')
-ax[1].scatter(aves_before_trim, ap_before_trim, alpha=utils.ALPHA, label='Before trim')
-ax[1].scatter(aves_after_trim, ap_after_trim, alpha=utils.ALPHA, label='After trim')
+ax[1].scatter(aves_before_trim, ap_before_trim, label='Before trim')
+ax[1].scatter(aves_after_trim, ap_after_trim, label='After trim')
 ax[1].set_xlabel('AVE')
 ax[1].set_ylabel('Average precision')
 ax[1].legend()
 ax[1].grid()
 utils.plot_fig_label(ax[1], 'B.')
 
+for a in ax:
+    a.axvline(0, linestyle= '--', c='k')
+    
 fig.savefig('./processed_data/graph_cluster/trim.png')
 fig.savefig('./processed_data/graph_cluster/trim.tif')
 plt.close(fig)
